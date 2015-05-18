80bffcb8bc12e0a55c38a56a3038179780de5e42
lestephen <lestephen@groupon.com>
2015-05-18 12:32:21 -0700

    Add first test (sanity test)

---
+++ src/test/java/com/github/lstephen/ootp/ai/TestSanity.java
@@ -0,0 +1,15 @@
+package com.github.lstephen.ootp.ai;
+
+import org.assertj.core.api.Assertions;
+
+import org.junit.Test;
+
+public class TestSanity {
+
+  @Test
+  public void shouldBeSane() {
+    Assertions.assertThat(true).isTrue();
+  }
+
+}
+
