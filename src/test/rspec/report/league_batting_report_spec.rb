0bd99a9b72a59ec6b42bd29735c4c1b0f4aec070
lestephen <lestephen@groupon.com>
2015-07-13 19:49:26 -0700

    Migrate tests to rspec

--- src/test/rspec/report/league_batting_report_spec.rb
+++ src/test/rspec/report/league_batting_report_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 require 'java'
 
-
 java_import com.ljs.ootp.ai.report.LeagueBattingReport
 java_import com.ljs.ootp.ai.site.Site;
 java_import com.ljs.ootp.ai.stats.BattingStats;
@@ -12,13 +11,17 @@ java_import java.io.StringWriter
 
 RSpec.describe LeagueBattingReport do
   let(:stats) { BattingStats.new }
-  let(:site) { double('Site', :getLeagueBatting => stats) }
+  let(:site) { double('Site', getLeagueBatting: stats) }
 
-  let(:out) { StringWriter.new }
-  let(:writer) { PrintWriter.new out }
+  subject(:report) { LeagueBattingReport.create site }
 
-  subject! { LeagueBattingReport.create(site).print(writer) }
+  describe '#print' do
+    let(:out) { StringWriter.new }
+    let(:writer) { PrintWriter.new out }
 
-  it { expect(out.to_s.empty?).to eq(false) }
+    subject! { report.print writer }
+
+    it { expect(out.to_s.empty?).to eq(false) }
+  end
 end
 
