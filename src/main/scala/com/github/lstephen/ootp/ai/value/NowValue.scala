7665d62247a3b72a8f632b85370a604746d3c50e
lstephen <levi.stephen@gmail.com>
2016-05-24 13:00:35 -0700

    Refactoring of now value

--- src/main/scala/com/github/lstephen/ootp/ai/value/NowValue.scala
+++ src/main/scala/com/github/lstephen/ootp/ai/value/NowValue.scala
@@ -4,29 +4,55 @@ import com.github.lstephen.ootp.ai.player.Player
 import com.github.lstephen.ootp.ai.player.ratings.Position
 import com.github.lstephen.ootp.ai.regression.Predictions
 import com.github.lstephen.ootp.ai.score._
+import com.github.lstephen.ootp.ai.selection.lineup.PlayerDefenseScore
 
 import collection.JavaConversions._
 
-class NowValue
-  (player: Player, position: Position)
-  (implicit predictions: Predictions)
+abstract class NowValue
+  (val player: Player, val position: Position)
+  (implicit val predictions: Predictions)
   extends Scoreable {
 
-  val pv = new PlayerValue(predictions, null, null)
+  val batting: Option[Score] = None
+  val pitching: Option[Score] = None
+  val defense: Option[Score] = None
 
-  val now = Score(pv.getNowValue(player).intValue)
-  val vsReplacement = ReplacementLevels.getForNow.get(player, position);
+  val vsReplacement = Some(ReplacementLevels.getForNow.get(player, position))
 
-  val score = now + vsReplacement
+  def components = List(batting, pitching, defense, vsReplacement)
+
+  def score: Score = components.map(_.getOrElse(Score.zero)).total
 
   def format: String =
-    f"${position.getAbbreviation}%2s/${now.toLong}%3d/${vsReplacement.toLong}%3d ${score.toLong}%3d"
+    components
+      .map(_.map(s => f"${s.toLong}%3d"))
+      .map(_.getOrElse("   "))
+      .mkString(f"${position.getAbbreviation}%2s : ", " ", f" : ${score.toLong}%3d")
 }
 
+trait BatterNowValue { this: NowValue =>
+  override val batting = Some(Score(predictions.getOverallHitting(player).intValue))
+  override val defense = Some(new PlayerDefenseScore(player, position).score)
+}
+
+trait PitcherNowValue { this: NowValue =>
+  override val pitching = Some(Score(predictions.getOverallPitching(player).intValue))
+}
+
+
 object NowValue {
+  def apply(p: Player, pos: Position)(implicit ps: Predictions): NowValue = {
+    if (p.isHitter()) {
+      return new NowValue(p, pos) with BatterNowValue
+    } else if (p.isPitcher()) {
+      return new NowValue(p, pos) with PitcherNowValue
+    }
+    throw new IllegalStateException
+  }
+
   def apply(p: Player)(implicit ps: Predictions): NowValue =
     (Position.hitting ++ Position.pitching)
-      .map(new NowValue(p, _))
+      .map(NowValue(p, _))
       .max
 }
 
