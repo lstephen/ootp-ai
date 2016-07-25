5a45cf20a8320e8b4134866e71ac96a309fa2844
lstephen <levi.stephen@gmail.com>
2016-07-25 09:07:12 +0300

    Set min instances per node of decision trees
    Revert to just using decision trees

--- src/main/scala/com/github/lstephen/ootp/ai/regression/spark.scala
+++ src/main/scala/com/github/lstephen/ootp/ai/regression/spark.scala
@@ -1,6 +1,9 @@
 package com.github.lstephen.ootp.ai.regression
 
 import org.apache.spark.mllib.tree.RandomForest
+import org.apache.spark.mllib.tree.configuration.Algo
+import org.apache.spark.mllib.tree.configuration.Strategy
+import org.apache.spark.mllib.tree.impurity.Variance
 
 import com.typesafe.scalalogging.StrictLogging
 
@@ -8,8 +11,17 @@ class RandomForestModel extends Model with StrictLogging {
   private val seed = 42
 
   def train(ds: DataSet) = {
-    val model = RandomForest.trainRegressor(
-      ds.toRdd, Map[Int, Int](), 100, "auto", "variance", 5, 32, seed)
+    val strategy = new Strategy(
+      algo = Algo.Regression,
+      impurity = Variance,
+      maxDepth = 5,
+      minInstancesPerNode = ds.length / 200
+      )
+
+    val model = RandomForest.trainRegressor(ds.toRdd, strategy, 100, "auto", seed)
+
+    //val model = RandomForest.trainRegressor(
+    //  ds.toRdd, Map[Int, Int](), 100, "auto", "variance", 5, 32, seed)
 
     logger.info(s"Model: $model")
 
