dd91aa409e6a59850efffbc739001831e7a7fcd9
lstephen <levi.stephen@gmail.com>
2016-11-30 20:55:10 -0800

    Dev Report refactoring

--- src/main/scala/com/github/lstephen/ootp/ai/report/DevelopmentReport.scala
+++ src/main/scala/com/github/lstephen/ootp/ai/report/DevelopmentReport.scala
@@ -16,26 +16,19 @@ import scala.collection.JavaConverters._
 
 class DevelopmentReport(site: Site, implicit val predictor: Predictor) extends Printable {
 
-  def playerIdSet(s: TeamStats[_]) = s.getAllRatings.asScala.toSet.map((p: Player) => p.getId)
-
-  val cHitting = site.getTeamBatting
-  val pHitting = History.create.loadBatting(site, -1)
-  val idsHitting = playerIdSet(cHitting).intersect(playerIdSet(pHitting))
-
-  val cPitching = site.getTeamPitching
-  val pPitching = History.create.loadPitching(site, -1)
-  val idsPitching = playerIdSet(cPitching).intersect(playerIdSet(pPitching))
+  def print(w: PrintWriter, pds: Seq[PlayerDevelopment], filter: PlayerDevelopment => Boolean) = {
+    pds.sortBy(pd => (pd.score, -pd.toP.getAge)).reverse.filter(filter).map(_.format).foreach(w.println(_))
+  }
 
   def print(w: PrintWriter): Unit = {
+    val dHitting = PlayerDevelopment.between(site.getTeamBatting, History.create.loadBatting(site, -1))
+    val dPitching = PlayerDevelopment.between(site.getTeamPitching, History.create.loadPitching(site, -1))
 
-    val dHitting = idsHitting.map(new PlayerDevelopment(_, pHitting, cHitting)).toList.sortBy(pd => (pd.score, - pd.toP.getAge())).reverse
-    val dPitching = idsPitching.map(new PlayerDevelopment(_, pPitching, cPitching)).toList.sortBy(pd => (pd.score, -pd.toP.getAge())).reverse
-
-    w.println("Hitters")
-    dHitting.filter(_.toP.isHitter).map(_.format).foreach(w.println(_))
+    w.format("%nHitters%n")
+    print(w, dHitting, _.toP.isHitter)
 
     w.format("%nPitchers%n")
-    dPitching.filter(_.toP.isPitcher).map(_.format).foreach(w.println(_))
+    print(w, dPitching, _.toP.isPitcher)
   }
 }
 
@@ -51,5 +44,17 @@ class PlayerDevelopment(pid: PlayerId, from: TeamStats[_], to: TeamStats[_])(imp
 
   val format =
       f"${toP.getListedPosition.or("")}%2s ${StringUtils.abbreviate(toP.getName(), 25)}%-25s | ${fromP.getAge}%2s ${fromV.format}%s | ${toP.getAge}%2s ${toV.format}%s | ${score.toLong}%+3d"
+}
+
+object PlayerDevelopment {
+
+  private def playerIdSet(s: TeamStats[_]) = s.getAllRatings.asScala.toSet.map((p: Player) => p.getId)
+
+  def between(from: TeamStats[_], to: TeamStats[_])(implicit predictor: Predictor): Seq[PlayerDevelopment] = {
+    val pids = playerIdSet(from).intersect(playerIdSet(to))
+
+    pids.map(new PlayerDevelopment(_, from, to)).toList
+  }
+
 }
 
