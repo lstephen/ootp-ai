2fdaedcfb0d3c58e1ebf3ac3464b4c39790aca3b
Levi Stephen <levi.stephen@gmail.com>
2019-06-19 12:02:24 +0300

    Update B coefficients for BsR

--- src/main/java/com/github/lstephen/ootp/ai/stats/BaseRuns.java
+++ src/main/java/com/github/lstephen/ootp/ai/stats/BaseRuns.java
@@ -3,11 +3,12 @@ package com.github.lstephen.ootp.ai.stats;
 /** @author lstephen */
 public interface BaseRuns {
 
-  double COEFFICIENT_SINGLE = 0.8;
-  double COEFFICIENT_DOUBLE = 2.1;
-  double COEFFICIENT_TRIPLE = 3.4;
-  double COEFFICIENT_HOME_RUN = 1.8;
-  double COEFFICIENT_WALK = 0.1;
+  // http://gosu02.tripod.com/id108.html
+  double COEFFICIENT_SINGLE = 0.78;
+  double COEFFICIENT_DOUBLE = 2.34;
+  double COEFFICIENT_TRIPLE = 3.9;
+  double COEFFICIENT_HOME_RUN = 2.34;
+  double COEFFICIENT_WALK = 0.39;
 
   Double calculate(PitchingStats stats);
 }
