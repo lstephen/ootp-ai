cfe91c4c9228f9302f5b2b6ae1a457bb5cf24a07
lstephen <levi.stephen@gmail.com>
2019-07-05 10:52:55 -0700

    Simpler lineup ordering. More emphasis on woba

--- src/main/java/com/github/lstephen/ootp/ai/selection/lineup/LineupOrdering.java
+++ src/main/java/com/github/lstephen/ootp/ai/selection/lineup/LineupOrdering.java
@@ -19,7 +19,9 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Ordering;
 import com.google.common.collect.Sets;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -29,207 +31,53 @@ import org.apache.commons.lang3.tuple.Pair;
 
 public class LineupOrdering {
 
-  private static final int WEIGHT_1B = 0;
-  private static final int WEIGHT_2B = 1;
-  private static final int WEIGHT_3B = 2;
-  private static final int WEIGHT_HR = 3;
-  private static final int WEIGHT_BB = 4;
-  private static final int WEIGHT_K = 7;
-  private static final int WEIGHT_OUT = 8;
-
-  private static final double[][] WEIGHTS = {
-    //  1B     2B    3B     HR    NIBB  HBP   RBOE      K     OUT
-    {.515, .806, 1.121, 1.421, .385, .411, .542, -.329, -.328},
-    {.515, .799, 1.100, 1.450, .366, .396, .536, -.322, -.324},
-    {.493, .779, 1.064, 1.452, .335, .369, .514, -.317, -.315},
-    {.517, .822, 1.117, 1.472, .345, .377, .541, -.332, -.327},
-    {.513, .809, 1.106, 1.438, .346, .381, .530, -.324, -.323},
-    {.482, .763, 1.050, 1.376, .336, .368, .504, -.306, -.306},
-    {.464, .738, 1.014, 1.336, .323, .353, .486, -.296, -.296},
-    {.451, .714, .980, 1.293, .312, .340, .470, -.287, -.286},
-    {.436, .689, .948, 1.249, .302, .329, .454, -.278, -.277}
-  };
-
-  private static final double[] RBIS_NO_DH = {
-    0.072, 0.090, 0.125, 0.133, 0.117, 0.108, 0.103, 0.083, 0.058
-  };
-  private static final double[] RBIS_DH = {
-    0.080, 0.098, 0.135, 0.126, 0.119, 0.103, 0.097, 0.093, 0.086
-  };
-
-  private static final double[] RISP = {.208, .255, .291, .305, .283, .266, .270, .270, .264};
-
-  private static final double[] PAS = {4.63, 4.52, 4.42, 4.32, 4.22, 4.11, 3.99, 3.88, 3.75};
-
-  private static final Map<Pair<Lineup.VsHand, ImmutableSet<Player>>, ImmutableList<Player>> CACHE =
-      Maps.newConcurrentMap();
-
   private final Predictor predictor;
 
   public LineupOrdering(Predictor predictor) {
     this.predictor = predictor;
   }
 
-  private Boolean isValid(Order order) {
-    return order.get().size() == ImmutableSet.copyOf(order.get()).size();
-  }
-
-  private Double score(Order order) {
-    Double vsL = score(order, VsHand.VS_LHP);
-    Double vsR = score(order, VsHand.VS_RHP);
-
-    return (vsL + vsR) * getBalanceFactor(order);
-  }
-
-  private Double getBalanceFactor(Order order) {
-    Double vsL = score(order, VsHand.VS_LHP) + 1;
-    Double vsR = score(order, VsHand.VS_RHP) + 1;
-
-    Double maxBalance = Math.pow((vsL + vsR) / 2.0, 2.0);
-    Double actBalance = vsL * vsR;
-
-    return Math.pow(actBalance / maxBalance, order.size());
-  }
-
-  private Double score(Order order, Lineup.VsHand vs) {
-    Double score = 0.0;
-    int pos = 0;
-
-    for (Player p : order.get()) {
-      score += score(p, pos, vs);
-      pos++;
-    }
-
-    return score;
-  }
-
-  private Double score(Player p, int pos, Lineup.VsHand vs) {
-    Double score = 0.0;
-
-    double[] ws = WEIGHTS[pos];
-
-    BattingStats ps = vs.getStats(predictor, p);
-
-    score += ws[WEIGHT_1B] * ps.getSinglesPerPlateAppearance();
-    score += ws[WEIGHT_2B] * ps.getDoublesPerPlateAppearance();
-    score += ws[WEIGHT_3B] * ps.getTriplesPerPlateAppearance();
-    score += ws[WEIGHT_HR] * ps.getHomeRunsPerPlateAppearance();
-    score += ws[WEIGHT_BB] * ps.getWalksPerPlateAppearance();
-    score += ws[WEIGHT_K] * ps.getKsPerPlateAppearance();
-    score += ws[WEIGHT_OUT] * (ps.getOutsPerPlateAppearance() - ps.getKsPerPlateAppearance());
-
-    return score;
-  }
-
-  private Ordering<Order> byScore(final Lineup.VsHand vs) {
-    return Ordering.natural()
-        .onResultOf(
-            new Function<Order, Double>() {
-              public Double apply(Order o) {
-                Double ovs = score(o, vs);
-                Double ovb = score(o);
-
-                Double early = ovs;
-                Double middle = ovs;
-                Double late = ovb;
-
-                return early + middle + late;
-              }
-            });
-  }
-
   public ImmutableList<Player> order(Lineup.VsHand vs, Iterable<Player> ps) {
     Preconditions.checkArgument(Iterables.size(ps) == 8 || Iterables.size(ps) == 9);
     Preconditions.checkArgument(Iterables.size(ps) == ImmutableSet.copyOf(ps).size());
 
-    Pair<Lineup.VsHand, ImmutableSet<Player>> key = Pair.of(vs, ImmutableSet.copyOf(ps));
+    Function<Function<BattingStats, Double>, Ordering<Player>> byStat = f ->
+      Ordering.natural().onResultOf(p -> f.apply(vs.getStats(predictor, p)));
 
-    if (CACHE.containsKey(key)) {
-      return CACHE.get(key);
+    Ordering<Player> byWoba = byStat.apply(BattingStats::getWoba);
+    Ordering<Player> byObp = byStat.apply(BattingStats::getOnBasePercentage);
+    Ordering<Player> byIso = byStat.apply(BattingStats::getIsoPower);
+
+    Player[] lineup = new Player[9];
+
+    Set<Player> available = new HashSet<>();
+    Iterables.addAll(available, ps);
+
+    // select top 5 for top 5 lineup spots
+    List<Player> best5 = byWoba.greatestOf(available, 5);
+    Iterables.removeAll(available, best5);
+
+    // best hitter at 4
+    lineup[3] = select(byWoba::max, best5); // best at 4
+    lineup[2] = select(byIso::max, best5);  // power at 3
+    lineup[4] = select(byObp::min, best5);  // lowest obp at 5
+    lineup[1] = select(byIso::max, best5);  // power at 2
+    lineup[0] = select(byWoba::max, best5); // remaining at 1
+
+    Preconditions.checkState(best5.isEmpty());
+
+    // put the rest in descending woba order
+    int pos = 5;
+    while (!available.isEmpty()) {
+      lineup[pos++] = select(byWoba::max, available);
     }
 
-    HillClimbing<Order> hc =
-        HillClimbing.<Order>builder()
-            .heuristic(byScore(vs))
-            .actionGenerator(new OrderActions())
-            .build();
-
-    Order result = new RepeatedHillClimbing<Order>(new RandomGenerator(ps), hc).search();
-
-    CACHE.put(key, result.get());
-
-    return result.get();
+    return ImmutableList.copyOf(lineup);
   }
 
-  private static final class Order {
-
-    private final ImmutableList<Player> order;
-
-    private Order(Iterable<Player> ps) {
-      this.order = ImmutableList.copyOf(ps);
-    }
-
-    public ImmutableList<Player> get() {
-      return order;
-    }
-
-    public Optional<Player> get(Integer idx) {
-      return idx >= 0 && idx < size() ? Optional.of(order.get(idx)) : Optional.<Player>absent();
-    }
-
-    public int size() {
-      return order.size();
-    }
-
-    public Order swap(int i, int j) {
-      List<Player> ps = Lists.newArrayList(order);
-      Collections.swap(ps, i, j);
-      return create(ps);
-    }
-
-    public static Order create(Iterable<Player> ps) {
-      return new Order(ps);
-    }
-  }
-
-  private static class RandomGenerator implements Supplier<Order> {
-
-    private List<Player> ps;
-
-    public RandomGenerator(Iterable<Player> ps) {
-      this.ps = Lists.newArrayList(ps);
-    }
-
-    public Order get() {
-      Collections.shuffle(ps);
-      return Order.create(ps);
-    }
-  }
-
-  private static class OrderActions implements ActionGenerator<Order> {
-    public Stream<Action<Order>> apply(Order o) {
-      Set<Action<Order>> actions = Sets.newHashSet();
-
-      for (int i = 0; i < o.size(); i++) {
-        for (int j = i + 1; j < o.size(); j++) {
-          actions.add(new Swap(i, j));
-        }
-      }
-
-      return Stream.concat(actions.stream(), SequencedAction.allPairs(actions));
-    }
-  }
-
-  private static class Swap implements Action<Order> {
-    private final int i, j;
-
-    public Swap(int i, int j) {
-      this.i = i;
-      this.j = j;
-    }
-
-    public Order apply(Order o) {
-      return o.swap(i, j);
-    }
+  private Player select(Function<Iterable<Player>, Player> f, Collection<Player> ps) {
+    Player selected = f.apply(ps);
+    ps.remove(selected);
+    return selected;
   }
 }
