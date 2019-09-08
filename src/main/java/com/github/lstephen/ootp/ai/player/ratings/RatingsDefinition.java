2a784eb4b8e4f60914c3bbcf5e36df5f9445d667
lstephen <levi.stephen@gmail.com>
2019-09-07 19:43:06 -0700

    Add ratings scales for VIV

--- src/main/java/com/github/lstephen/ootp/ai/player/ratings/RatingsDefinition.java
+++ src/main/java/com/github/lstephen/ootp/ai/player/ratings/RatingsDefinition.java
@@ -3,7 +3,5 @@ package com.github.lstephen.ootp.ai.player.ratings;
 /** @author lstephen */
 public interface RatingsDefinition {
 
-  Double getYearlyRatingsIncrease();
-
   Boolean isFreezeOneRatings();
 }
